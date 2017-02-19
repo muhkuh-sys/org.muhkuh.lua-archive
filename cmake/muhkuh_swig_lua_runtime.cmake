86cddfb8b918f58bfb85a209dd1cff4fe6cf699f
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-19 21:31:51 +0100

    Add the build scripts for the wrapper and SWIG binding.

---
+++ cmake/muhkuh_swig_lua_runtime.cmake
@@ -0,0 +1,16 @@
+#----------------------------------------------------------------------------
+#
+# Add a custom target for the swig runtime includes.
+# The runtimes are generated during the build process by running the swig
+# executable.
+#
+
+# Create a new directory for the swig runtime.
+SET(SWIG_RUNTIME_OUTPUT_PATH ${CMAKE_BINARY_DIR}/swig_runtime)
+MAKE_DIRECTORY(${SWIG_RUNTIME_OUTPUT_PATH})
+
+# TODO: Replace this and all other custom commands with ExternalProject ?
+ADD_CUSTOM_TARGET(TARGET_swigluarun "${SWIG_EXECUTABLE}" -lua -external-runtime ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
+                  COMMENT "Build the swig lua runtime header."
+                  VERBATIM
+)
