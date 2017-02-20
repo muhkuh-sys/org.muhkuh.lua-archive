5711e8fc2daef9199ef049e88e1fea9bba83ec22
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-20 16:06:17 +0100

    Add the "swigluarun" target to the global target.

--- cmake/muhkuh_swig_lua_runtime.cmake
+++ cmake/muhkuh_swig_lua_runtime.cmake
@@ -10,7 +10,9 @@ SET(SWIG_RUNTIME_OUTPUT_PATH ${CMAKE_BINARY_DIR}/swig_runtime)
 MAKE_DIRECTORY(${SWIG_RUNTIME_OUTPUT_PATH})
 
 # TODO: Replace this and all other custom commands with ExternalProject ?
-ADD_CUSTOM_TARGET(TARGET_swigluarun "${SWIG_EXECUTABLE}" -lua -external-runtime ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
+ADD_CUSTOM_TARGET(TARGET_swigluarun ALL
+                  COMMAND "${SWIG_EXECUTABLE}" -lua -external-runtime ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
                   COMMENT "Build the swig lua runtime header."
+                  BYPRODUCTS ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
                   VERBATIM
 )
