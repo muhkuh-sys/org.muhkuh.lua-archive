a76b507f12c7217a1a611909ee52cbc73d16d426
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-04-26 12:47:41 +0200

    Add a script to build all artifacts.

---
+++ .lxc_build_all.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+. .lxc_build_windows.sh
+. .lxc_build_ubuntu_1404_x64.sh
+. .lxc_build_ubuntu_1404_x86.sh
+. .lxc_build_ubuntu_1604_x64.sh
+. .lxc_build_ubuntu_1604_x86.sh
+. .lxc_build_ubuntu_1610_x64.sh
+. .lxc_build_ubuntu_1610_x86.sh
+. .build04_artefacts.sh
