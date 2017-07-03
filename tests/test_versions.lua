03389a98d29a3f8951dbb4f0486d250e6d423b46
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-07-03 12:44:57 +0200

    Detect if a library was not build.

--- tests/test_versions.lua
+++ tests/test_versions.lua
@@ -5,7 +5,13 @@ print(string.format("version number: %d", archive.version_number()))
 print(string.format("version string: %s", archive.version_string()))
 print(string.format("version details: %s", archive.version_details()))
 print()
-print(string.format("zlib version: %s", archive.zlib_version()))
-print(string.format("liblzma version: %s", archive.liblzma_version()))
-print(string.format("bzlib version: %s", archive.bzlib_version()))
-print(string.format("liblz4 version: %s", archive.liblz4_version()))
+
+local strZLIB_version = archive.zlib_version() or "none"
+local strLIBLZMA_version = archive.liblzma_version() or "none"
+local strBZLIB_version = archive.bzlib_version() or "none"
+local strLIBLZ4_version = archive.liblz4_version() or "none"
+
+print(string.format("zlib version: %s", strZLIB_version))
+print(string.format("liblzma version: %s", strLIBLZMA_version))
+print(string.format("bzlib version: %s", strBZLIB_version))
+print(string.format("liblz4 version: %s", strLIBLZ4_version))
