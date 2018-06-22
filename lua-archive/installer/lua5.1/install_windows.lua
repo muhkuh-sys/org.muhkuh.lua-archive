e5c516a8f937f337eb93b5d6364d5d7dab13f5d1
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-22 16:22:25 +0200

    Get the tool path from the jonchkihere script.

--- lua-archive/installer/lua5.1/install_windows.lua
+++ lua-archive/installer/lua5.1/install_windows.lua
@@ -2,7 +2,7 @@ local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
-if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' then
+if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
   t:install('lua_plugins/archive.dll', '${install_lua_cpath}/')
   tResult = true
 end
