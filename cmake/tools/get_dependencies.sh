2eb808fb82aa5ab2a4e7fb8200ebf9b36ad446f3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-11-26 11:28:12 +0100

    Update all absolute symlinks.

--- cmake/tools/get_dependencies.sh
+++ cmake/tools/get_dependencies.sh
@@ -3,11 +3,11 @@
 PKG_DIR=$(pwd)
 apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $1 | grep "^\w")
 find . -name "*.deb" -exec dpkg-deb --extract '{}' . \;
-ALLLINKS=$(find . -xtype l)
+ALLLINKS=$(find . -type l)
 for LINK in ${ALLLINKS}
 do
 	TARGET=$(readlink ${LINK})
-	if [ ! -f ${TARGET} ]; then
+	if [[ "${TARGET}" =~ ^/.* ]]; then
 		echo "Updating symlink ${LINK} to ${PKG_DIR}${TARGET}"
 		ln -fs ${PKG_DIR}${TARGET} ${LINK}
 	fi
