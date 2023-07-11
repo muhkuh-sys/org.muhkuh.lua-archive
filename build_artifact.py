2acf9bf958791e1bf74c90c1e5c275c025403123
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-07-11 13:11:55 +0200

    Drop support for LUA5.1 .

--- build_artifact.py
+++ build_artifact.py
@@ -64,14 +64,6 @@ strCfg_jonchkiProjectConfiguration = os.path.join(
     'jonchki',
     'jonchkicfg.xml'
 )
-strCfg_jonchkiDependencyLog51 = os.path.join(
-    strCfg_projectFolder,
-    'dependency-log-lua5.1.xml'
-)
-strCfg_jonchkiDependencyLog54 = os.path.join(
-    strCfg_projectFolder,
-    'dependency-log-lua5.4.xml'
-)
 
 # -
 # --------------------------------------------------------------------------
@@ -264,8 +256,6 @@ else:
 astrFolders = [
     strCfg_workingFolder,
     os.path.join(strCfg_workingFolder, 'external'),
-    os.path.join(strCfg_workingFolder, 'lua5.1'),
-    os.path.join(strCfg_workingFolder, 'lua5.1', 'build_requirements'),
     os.path.join(strCfg_workingFolder, 'lua5.4'),
     os.path.join(strCfg_workingFolder, 'lua5.4', 'build_requirements'),
 ]
@@ -282,89 +272,7 @@ strJonchki = jonchkihere.install(
 
 # ---------------------------------------------------------------------------
 #
-# Get the build requirements for LUA5.1 and the externals.
-#
-strCwd = os.path.join(strCfg_workingFolder, 'lua5.1', 'build_requirements')
-for strMatch in glob.iglob(os.path.join(strCwd, 'lua5.1-archive-*.xml')):
-    os.remove(strMatch)
-
-astrCmd = [
-    'cmake',
-    '-DCMAKE_INSTALL_PREFIX=""',
-    '-DPRJ_DIR=%s' % strCfg_projectFolder,
-    '-DWORKING_DIR=%s' % strCfg_workingFolder,
-    '-DBUILDCFG_ONLY_JONCHKI_CFG="ON"',
-    '-DBUILDCFG_LUA_USE_SYSTEM="OFF"',
-    '-DBUILDCFG_LUA_VERSION="5.1"'
-]
-astrCmd.extend(astrCMAKE_COMPILER)
-astrCmd.extend(astrCMAKE_PLATFORM)
-astrCmd.append(strCfg_projectFolder)
-subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
-subprocess.check_call(strMake, shell=True, cwd=strCwd, env=astrEnv)
-
-astrMatch = glob.glob(os.path.join(strCwd, 'lua5.1-archive-*.xml'))
-if len(astrMatch) != 1:
-    raise Exception('No match found for "lua5.1-archive-*.xml".')
-
-astrCmd = [
-    strJonchki,
-    'install-dependencies',
-    '--verbose', strCfg_jonchkiVerbose,
-    '--syscfg', strCfg_jonchkiSystemConfiguration,
-    '--prjcfg', strCfg_jonchkiProjectConfiguration,
-    '--logfile', strCfg_jonchkiLog51,
-    '--dependency-log', strCfg_jonchkiDependencyLog51
-]
-astrCmd.extend(astrJONCHKI_SYSTEM)
-astrCmd.append('--build-dependencies')
-astrCmd.append(astrMatch[0])
-subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
-
-astrCMAKE_COMPILER.append('-Dnet.zlib-zlib_DIR=%s' % os.path.join(strCfg_workingFolder, 'lua5.1', 'build_requirements', 'jonchki', 'install', 'dev', 'cmake'))
-
-# ---------------------------------------------------------------------------
-#
-# Build the externals.
-#
-astrCmd = [
-    'cmake',
-    '-DCMAKE_INSTALL_PREFIX=""',
-    '-DPRJ_DIR=%s' % strCfg_projectFolder,
-    '-DWORKING_DIR=%s' % strCfg_workingFolder
-]
-astrCmd.extend(astrCMAKE_COMPILER)
-astrCmd.append(os.path.join(strCfg_projectFolder, 'external'))
-strCwd = os.path.join(strCfg_workingFolder, 'external')
-subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
-subprocess.check_call(strMake, shell=True, cwd=strCwd, env=astrEnv)
-subprocess.check_call('%s install' % strMake, shell=True, cwd=strCwd, env=astrEnv)
-
-astrCMAKE_COMPILER.append('-DEXTERNAL_LIB_DIR=%s' % os.path.join(strCfg_workingFolder, 'external', 'install', 'lib'))
-astrCMAKE_COMPILER.append('-DEXTERNAL_INCLUDE_DIR=%s' % os.path.join(strCfg_workingFolder, 'external', 'install', 'include'))
-
-# ---------------------------------------------------------------------------
-#
-# Build the LUA5.1 version.
-#
-astrCmd = [
-    'cmake',
-    '-DCMAKE_INSTALL_PREFIX=""',
-    '-DPRJ_DIR=%s' % strCfg_projectFolder,
-    '-DWORKING_DIR=%s' % strCfg_workingFolder,
-    '-DBUILDCFG_LUA_USE_SYSTEM="OFF"',
-    '-DBUILDCFG_LUA_VERSION="5.1"'
-]
-astrCmd.extend(astrCMAKE_COMPILER)
-astrCmd.extend(astrCMAKE_PLATFORM)
-astrCmd.append(strCfg_projectFolder)
-strCwd = os.path.join(strCfg_workingFolder, 'lua5.1')
-subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
-subprocess.check_call('%s pack' % strMake, shell=True, cwd=strCwd, env=astrEnv)
-
-# ---------------------------------------------------------------------------
-#
-# Get the build requirements for LUA5.4.
+# Get the build requirements for LUA5.4 and the externals.
 #
 strCwd = os.path.join(strCfg_workingFolder, 'lua5.4', 'build_requirements')
 for strMatch in glob.iglob(os.path.join(strCwd, 'lua5.4-archive-*.xml')):
@@ -395,14 +303,45 @@ astrCmd = [
     '--verbose', strCfg_jonchkiVerbose,
     '--syscfg', strCfg_jonchkiSystemConfiguration,
     '--prjcfg', strCfg_jonchkiProjectConfiguration,
-    '--logfile', strCfg_jonchkiLog54,
-    '--dependency-log', strCfg_jonchkiDependencyLog54
+
+    '--logfile', os.path.join(
+        strCfg_workingFolder,
+        'lua5.4',
+        'build_requirements',
+        'jonchki.log'
+    ),
+
+    '--dependency-log', os.path.join(
+        strCfg_projectFolder,
+        'dependency-log.xml'
+    )
 ]
 astrCmd.extend(astrJONCHKI_SYSTEM)
 astrCmd.append('--build-dependencies')
 astrCmd.append(astrMatch[0])
 subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
 
+astrCMAKE_COMPILER.append('-Dnet.zlib-zlib_DIR=%s' % os.path.join(strCfg_workingFolder, 'lua5.4', 'build_requirements', 'jonchki', 'install', 'dev', 'cmake'))
+
+# ---------------------------------------------------------------------------
+#
+# Build the externals.
+#
+astrCmd = [
+    'cmake',
+    '-DCMAKE_INSTALL_PREFIX=""',
+    '-DPRJ_DIR=%s' % strCfg_projectFolder,
+    '-DWORKING_DIR=%s' % strCfg_workingFolder
+]
+astrCmd.extend(astrCMAKE_COMPILER)
+astrCmd.append(os.path.join(strCfg_projectFolder, 'external'))
+strCwd = os.path.join(strCfg_workingFolder, 'external')
+subprocess.check_call(' '.join(astrCmd), shell=True, cwd=strCwd, env=astrEnv)
+subprocess.check_call(strMake, shell=True, cwd=strCwd, env=astrEnv)
+subprocess.check_call('%s install' % strMake, shell=True, cwd=strCwd, env=astrEnv)
+
+astrCMAKE_COMPILER.append('-DEXTERNAL_LIB_DIR=%s' % os.path.join(strCfg_workingFolder, 'external', 'install', 'lib'))
+astrCMAKE_COMPILER.append('-DEXTERNAL_INCLUDE_DIR=%s' % os.path.join(strCfg_workingFolder, 'external', 'install', 'include'))
 
 # ---------------------------------------------------------------------------
 #
